fcf400330b2fcf03f2cc69a80085934d7dc8f0b1
DalerKhusainov <dalerkhusainov85@gmail.com>
2026-04-24 17:18:54 +0500

    removed all folders for restarting a practice on nodejs

--- src/math.ts
+++
@@ -1,8 +0,0 @@
-function add(a: number, b: number) {
-  return a + b;
-}
-
-export = add;
-
-//
-// "dev": "nodemon --exec ts-node src/index.ts",
--- types/user.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-  id: number;
-  name: string;
-  email: string;
-}
-
-export type CreateUserDTO = Omit<User, "id">; // DTO для создания
-export type UpdateUserDTO = Partial<Omit<User, "id">>; // >DTO для обновления
